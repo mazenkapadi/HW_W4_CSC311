45cbacd762d66291eedcc60208afe9c1eaf01ad8
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2023-09-20 17:14:32 -0400

    HW_W4_CSC311-1 Complete Intern class

--- src/main/java/com/mycompany/agency/Intern.java
+++ src/main/java/com/mycompany/agency/Intern.java
@@ -4,22 +4,20 @@ package com.mycompany.agency;
 
     // TODO 1: Make Intern a child of StaffEmploee
 
-public class Intern 
+public class Intern extends StaffEmployee
 {
     //-----------------------------------------------------------------
     //  TODO 2: Constructor: Sets up this intern using the specified
     //  information.
     //-----------------------------------------------------------------
-    public Intern(String eName, String eAddress, String ePhone)
-    {
-        
+    public Intern(String eName, String eAddress, String ePhone) {
+        super(eName, eAddress, ePhone);
     }
 
     //-----------------------------------------------------------------
     // TODO 3:  Returns a zero pay value for this intern.
     //-----------------------------------------------------------------
-    public double pay()
-    {
-      
+    public double pay() {
+        return 0.0;
     }
 }
